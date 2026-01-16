9e3e7c557660550c68812e1c93ac90da170fc127
Mariama Sakho <mariama.sakho@gmail.com>
2026-01-16 19:12:12 +0100

    Add Cloudinary image upload for lists + cinema UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/create'
   root to: "lists#index"
 
   resources :lists, only: %i[index show new create] do
